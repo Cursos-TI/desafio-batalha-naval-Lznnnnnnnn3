0e758d62659e662dbfee021682f35485e0fee516
Lznnnnnnnn3 <luizoliveira12512@gmail.com>
2025-04-18 03:24:49 +0000

    Projeto Finalizado.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,69 +1,88 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main() {
 
-    //Matrizes do tabuleiro!!
-    int i, j;
-    int LINHA2 [10] = { //Segunda Linha Vertical
-        { 1},
-        { 2},
-        { 3},
-        { 4},
-        { 5},
-        { 6},
-        { 7},
-        { 8},
-        { 9},
-        {10}};
+    const int linhas   = 10;
+    const int colunas  = 10;
+    int tabuleiro[linhas][colunas];
+    char linhaHORIZONTAL [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; //Linha Horizontal 
 
-    char LINHA [] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; //Linha Horizontal 
-
-    int Tabuleiro [10][10] = {        //Tabuleiro
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 3, 3, 3, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    };
-
-    //
-    printf("     Batalha naval!!\n");
-
-    //Montagem do Tabuleiro
-    printf(" ");
-
-    for (int i = 0; i < 10; i++)  //Aplicação da linha Horizontal
-    {
-        
-        printf(" %c", LINHA[i]);
-    }
-    
-    printf("\n");
+    // 1. Inicializa tudo com água (0)
+    for (int i = 0; i < linhas; i++){
+        for (int j = 0; j < colunas; j++){
+            tabuleiro[i][j] = 0;}
+        }   
 
 
+    // 2. Exibição do navio em forma de cone
+    int altura      = 3;
+    int linha_off   = 4;   // ex.: inicia na linha 4
+    int col_off     = -3;  // ex.: desloca 2 colunas à esquerda do centro
 
-    for (int i = 0; i < 10; i++) //Aplicação da linha Vertical e o Tabuleiro
-    {
-        printf("%d", LINHA2[i]);
-        printf(" ");
-        for ( j = 0; j < 10; j++)
-        {
-            printf("%d ", Tabuleiro[i][j]);
+    for (int i = 0; i < altura; i++) {
+        int linha  = i + linha_off;
+        int meio   = colunas/2 + col_off;
+        int inicio = meio - i;
+        int fim    = meio + i;
+        for (int j = inicio; j <= fim; j++) {
+            if (linha >= 0 && linha < linhas && j >= 0 && j < colunas)
+                tabuleiro[linha][j] = 3;
         }
-        printf("\n");
     }
-    
-    
-    
-   
-    
 
-    
+    //3. Exibição do navio em forma de ecteadro
+    int arm = 1;
+    int center_row = linhas - 8;    // uma linha acima do último índice
+    int center_col = colunas - 3;   // uma coluna antes da última coluna
 
-    return 0;
+    // vertical
+    for (int k = -arm; k <= arm; k++) {
+        int r = center_row + k;
+        if (r >= 0 && r < linhas)
+            tabuleiro[r][center_col] = 2;
+    }
+    // horizontal
+    for (int k = -arm; k <= arm; k++) {
+        int c = center_col + k;
+        if (c >= 0 && c < colunas)
+            tabuleiro[center_row][c] = 2;
+    }
+
+    //4. Exibição do navio em forma de cruz
+    int arm2 = 2;
+    int center_row2 = linhas - 3;    // uma linha acima do último índice
+    int center_col2 = colunas - 4;   // uma coluna antes da última coluna
+
+    // vertical
+    for (int k = -arm2; k <= arm2; k++) {
+        int r = center_row2 + k;
+        if (r >= 0 && r < linhas)
+            tabuleiro[r][center_col2] = 1;
+    }
+    // horizontal
+    for (int k = -arm2; k <= arm2; k++) {
+        int c = center_col2 + k;
+        if (c >= 0 && c < colunas)
+            tabuleiro[center_row2][c] = 1;
+    }
+
+
+    // 5. Imprime o tabuleiro
+    printf("   ");
+    for (int i = 0; i < linhas; i++)//Exibição da linha Horizontal
+    {
+        printf("%c ", linhaHORIZONTAL[i]);
+    }
+    printf("\n");
+    
+    for (int i = 0; i < linhas; i++){ 
+    printf("%2d ", i + 1); //Exbição da linha vertical
+    
+        for (int j = 0; j < colunas; j++){ //Exibição dos numeros 0 (Água)
+            printf("%d ", tabuleiro[i][j]);
+    }
+    printf("\n");
+}
+
+return 0;
 }
